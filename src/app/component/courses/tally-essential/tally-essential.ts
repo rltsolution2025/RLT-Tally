a4f944413b63f63b97d60b369814b7e1dd86e37c
rltsolution2025 <social.rltdigital@gmail.com>
2026-03-17 11:53:21 +0530

    Program added

--- src/app/component/courses/tally-essential/tally-essential.ts
+++ src/app/component/courses/tally-essential/tally-essential.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tally-essential',
-  imports: [],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './tally-essential.html',
   styleUrl: './tally-essential.css',
 })
 export class TallyEssential {
 
+  enrollForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.enrollForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      message: ['']
+    });
+  }
+
+  submitForm(){
+    if(this.enrollForm.valid){
+      console.log(this.enrollForm.value);
+      alert('Enrollment successful!');
+      this.enrollForm.reset();
+    }
+    else{
+      alert('Please fill in all required fields correctly.');
+    }
+  }
 }
