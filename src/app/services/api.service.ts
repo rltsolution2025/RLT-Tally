49d41fdcc4be880ca065d340660d47dd3109ebfd
rltsolution2025 <social.rltdigital@gmail.com>
2026-06-09 15:01:19 +0530

    Added Environmental files

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,29 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = 'https://rlt-tally-1.onrender.com/api'; 
-  constructor(private http: HttpClient) { }
+  private baseUrl = environment.apiUrl;
 
-  //Enroll
+  constructor(private http: HttpClient) {}
+
+  // Enroll
   enroll(data: any) {
     return this.http.post(`${this.baseUrl}/enroll`, data);
   }
 
-  //Career
+  // Career
   career(data: any) {
     return this.http.post(`${this.baseUrl}/career`, data);
   }
 
-  //Contact
+  // Contact
   contact(data: any) {
     return this.http.post(`${this.baseUrl}/contact`, data);
   }
 
-  //Register
+  // Register
   register(data: any) {
     return this.http.post(`${this.baseUrl}/register`, data);
   }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://rlt-tally-1.onrender.com/api',
+};
